76cf51e55858a9074593b47b0a592c92dc71ab35
n-s-iroagba <nnamdisolomon1@gmail.com>
2025-06-18 17:40:28 +0100

    [chore]: started implementing useApplicationRequirementHook.ts

---
+++ client/src/hooks/useApplicationRequirements.ts
@@ -0,0 +1,9 @@
+const useApplicationRequirments = ()=>{
+    const [sessionData, setSessionData] = useState<SessionCreationDto[]>([]);
+    const [facultyData, setFacultyData] = useState<FacultyCreationDto[]>([]);
+    const [departmentData, setDepartmentData] = useState<DepartmentCreationDto[]>([]);
+    const [sscRequirmentsData, setSCCRequirmentsData] = useState<SSCRequirmentCreationDto[]>([]);
+    const [programSpecificRequirementsData, setProgramSpecificRequirementsData] = useState<ProgramSpecificRequirementsreationDto[]>([]);
+     
+
+}
